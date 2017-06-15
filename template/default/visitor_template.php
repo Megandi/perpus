2278d308ef7733b1aa9c8c10bfdeaa0f148b21d7
megandi <me.gandi@rocketmail.com>
2017-06-15 14:00:09 +0700

    visitor

--- template/default/visitor_template.php
+++ template/default/visitor_template.php
@@ -1,97 +1,71 @@
 <?php
-/**
- * Template for visitor counter
- * name of memberID text field must be: memberID
- * name of institution text field must be: institution
- *
- * Copyright (C) 2015 Arie Nugraha (dicarve@gmail.com)
- * Create by Eddy Subratha (eddy.subratha@slims.web.id)
- * 
- * Slims 8 (Akasia)
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-$main_template_path = $sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/login_template.inc.php';
-
+    $main_template_path = $sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/login_template.inc.php';
 ?>
+
+<script type="text/javascript" src="template/default/visitor_template_js.js"></script>
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        text-align: left;
+        padding: 8px;
+        font-family: Helvetica;
+    }
+    tr:nth-child(even){background-color: #f2f2f2}
+</style>
+
 <div class="s-visitor container">
-  <header>
-    <h1><?php echo __('Visitor Counter'); ?></h1>
-    <div  id="counterInfo" class="info"><?php echo __('Please insert your library member ID otherwise your full name instead'); ?></div>
-    <div class="s-visitor-photo">  
-      <img id="visitorCounterPhoto" src="./images/persons/photo.png"/>
-    </div>
-  </header>
-  
-  <form action="index.php?p=visitor" name="visitorCounterForm" id="visitorCounterForm" method="post">
-
-    <div class="row"> 
-
-      <div class="col-lg-4 col-sm-4 col-xs-12">
-        <div class="form-group"> 
-          <input type="text" name="memberID" id="memberID"  class="form-control input-lg" />
-          <label for="memberID"><?php echo __('Member ID / Visitor Name'); ?></label>
-        </div> 
-      </div>
-
-      <div class="col-lg-4 col-sm-4 col-xs-12">
-        <div class="form-group"> 
-          <input type="text" name="institution" id="institution" class="form-control input-lg" />
-          <label for="institution"><?php echo __('Institution'); ?></label>
-        </div> 
-      </div>
-
-            <div class="col-lg-4 col-sm-4 col-xs-12">
-        <div class="form-group"> 
-          <input type="text" name="locker" id="locker" class="form-control input-lg" />
-          <label for="locker"><?php echo __('Number Locker'); ?></label>
-        </div> 
-      </div>
-    
-      <div class="clearfix"></div>
-
-      <div class="col-lg-12 col-sm-12 col-xs-12">
-        <input type="submit" id="counter" name="counter" class="form-control input-lg" value="<?php echo __('Add'); ?>">
-      </div>
-      </div>
-    </div>
-
-  </form>
-
+    <header>
+        <h1>Visitor Counter</h1>
+        <div class="info">Please tap your RFID card</div>
+        <div class="row">
+        </div>
+    </header>
+    <form action="index.php?p=visitor" name="visitorCounterForm" id="visitorCounterForm" method="post" style="padding-top:50px;">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 col-xs-12">
+                <input type="text" name="memberid" id="memberid" class="form-control input-lg" onkeypress="return runScript(event)"/>
+                <label for="locker">Your ID card should writed in there</label>
+            </div>
+        </div>
+    </form>
 </div>
 
-<div class="s-visitor container" style="margin-top: 10px; margin-bottom: 70px; border-top: dashed 1px #ffffff; padding-top: 10px;  ">
-  <form action="index.php?p=visitor" name="returnlockerForm" id="returnlockerForm" method="post">
-     <div class="row"> 
-     <div class="col-lg-12 col-sm-12 col-xs-12">
-        <div class="form-group"> 
-          <input type="text" name="locker" id="locker_s" class="form-control input-lg" />
-          <label for="locker"><?php echo __('Number Locker'); ?></label>
-        </div> 
-      </div>
-    
-      <div class="clearfix"></div>
 
-      <div class="col-lg-12 col-sm-12 col-xs-12">
-        <input type="submit" id="counter" name="counter" class="form-control input-lg" value="<?php echo __('Locker Return'); ?>">
-      </div>
-     </div>
-  </form>
+<div class="modal fade" id="rejectModalTap" tabindex="-1" role="dialog" aria-labelledby="rejectModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color:#1abc9c">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" style="color:white">Visitor Detail</h4>
+            </div>
+	        <div class="modal-body" style="color:black">
+                <div class="row" style="margin:10px;">
+                    <table>
+                          <tr>
+                            <th style="background-color: #1abc9c;color: white;width:30%;border-bottom: 0.5px solid #1ae0b9;"><h5>Member ID</h5></th>
+                            <th><h5 id="memberid2">105216053</h5></th>
+                          </tr>
+                          <tr>
+                            <th style="background-color: #1abc9c;color: white;border-bottom: 1px solid #1ae0b9;"><h5>Nama</h5></th>
+                            <th><h5 id="name">Megandi</h5></th>
+                          </tr>
+                          <tr>
+                            <th style="background-color: #1abc9c;color: white;border-bottom: 1px solid #1ae0b9;"><h5>Member's Email</h5></th>
+                            <th style="border-bottom: 1px solid #ddd;"><h5 id="email">Standart</h5></th>
+                          </tr>
+                          <tr>
+                            <th style="background-color: #1abc9c;color: white;border-bottom: 1px solid #1ae0b9;"><h5>Number Locker</h5></th>
+                            <th style="border-bottom: 1px solid #ddd;"><input type="text" class="form-control input-md" onkeypress="return runScript(event)"/></th>
+                          </tr>
+                    </table>
+                </div>
+	        </div>
+            <div class="modal-footer">
+                <button id="submit" class="btn btn-primary">Save Locker</button>
+            </div>
+        </div>
+    </div>
 </div>
-<script>
-  $('#login-page, .s-login').attr('style','margin:0;')
-  $('.s-login-content').removeClass('animated flipInY').addClass('animated fadeInUp')
-</script>
