b6442bc8b299ad54c25106a176ba3f90731f6331
megandi <me.gandi@rocketmail.com>
2017-06-22 12:43:06 +0700

    test

--- template/default/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "root"); // db password
-define('DB_DATABASE', "perpus"); // database name
-define('DB_SERVER', "localhost"); // db server
-?>
